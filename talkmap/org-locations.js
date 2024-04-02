1b86fb4130668334eae774316ef9e5d57b977546
iamhankai <kaihana@163.com>
2024-04-02 23:40:56 +0800

    update talks

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,5 +1,10 @@
 var addressPoints = [
   [
+    "Macau, China",
+    22.1899448,
+    113.5380454
+  ],
+  [
     "Beijing, China",
     39.9057136,
     116.3912972
@@ -8,6 +13,11 @@ var addressPoints = [
     "Stockholm, Sweden",
     59.3251172,
     18.0710935
+  ],
+  [
+    "Shenzhen, China",
+    22.5445741,
+    114.0545429
   ],
   [
     "Taipei, Republic of China",
